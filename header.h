166b4b6d98523e05bbf166a278b263fd99bfd873
karonboi <94039272+karonboi@users.noreply.github.com>
2026-05-01 22:02:58 +0700

    Refactor header.h for improved variable types and clarity

--- header.h
+++ header.h
@@ -1,4 +1,4 @@
-//Declare Everything Here
+// Everything used in "main.cpp" will be declared here
 
 #ifndef HEADER_H
 #define HEADER_H
@@ -6,57 +6,58 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <queue>
 using namespace std;
 
-//Variables
-const int MedBoxCols = 4; // Số cột của MedBox
-const int MedBoxRows = 4; // Số hàng của MedBox
-const int MaxMedPrescription = 5; // Số lượng thuốc tối đa trong một đơn thuốc
+// Initiating common variables
+const int MedBoxCols = 4;          // Medical Box display columns
+const int MedBoxRows = 4;          // Medical Box display rows
+const int MaxMedPrescription = 5;  // Maximum number of medicines per prescription
 const string Filename = "MedicalRecord.bin";
 
 
-//Structs
-struct Patient{
-    string name;
+// Building essential structs
+struct Patient {
+    char name[100];
     int age;
     int height;
     int weight;
-    string illness;
+    char illness[100];
 };
 
-struct Medicine{
+struct Medicine {
     int amount;
-    string name;
+    char name[50];
 };
 
-struct MedicalRecord{
+struct MedicalRecord {
     Patient patient;
     char diagnosis[100];
     int medAmount = 0;
     Medicine prescription[MaxMedPrescription]; // Patient get maximum 5 medicines per prescription
 };
 
-//Linked List
-struct MedicalRecordNode{
+// Creating linked lists
+struct MedicalRecordNode {
     MedicalRecord record;
     MedicalRecordNode* next;
 };
-struct MedicalRecordList{
+struct MedicalRecordList {
     MedicalRecordNode* head;
 };
 
 
-//Functions Declaration
+// Declaring common functions
 Patient inputPatient();
 void displayPatient(const Patient& patient);
 void InitMedBox();
 void InitPatientQueue();
-bool checkMedicineAvailability(string MedName, int MedAmount);
+bool checkMedicineAvailability(const char* MedName, int MedAmount);
 MedicalRecord TreatingPatient();
 void displayMedicalRecord(const MedicalRecord& record);
 void AddMedicalRecord(MedicalRecordList &list, MedicalRecord newRecord);
 void LoadRecordsFromFile(MedicalRecordList &list, string fileName);
 void SaveRecordsToFile(MedicalRecordList list, string fileName);
 void clrscr();
-#endif // HEADER_H
+#endif // End of HEADER_H declaration
